9daf83b6b07757b7d135a3369772066c9f3280dc
Jisu Lee <56678959+egsu20@users.noreply.github.com>
2020-12-26 23:58:16 +0900

    Revert "Revert "Revert "혼공파"""
    This reverts commit b72046c40479ccbde1aa71cb3eea2c909ea4a19d.

--- Python/hgp/boolean.py
+++
@@ -1,4 +0,0 @@
-x = 10
-under_2 = x < 20
-print(under_2)
-print(not under_2)
--- Python/hgp/file_open.py
+++
@@ -1,7 +0,0 @@
-import random
-
-hanguls = list("가나다라마바사아자차카타파하")
-
-with open("information.txt","w") as file:
-    for i in range(100):
-        name = random.choice(list)
