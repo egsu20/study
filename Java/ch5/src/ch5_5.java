2381495b82f45014fb5dad5691edb45e490ef859
Jisu Lee <56678959+egsu20@users.noreply.github.com>
2021-01-29 11:34:05 +0900

    1.29 자바에센셜 5장

---
+++ Java/ch5/src/ch5_5.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+interface StackInterface{
+	int length();
+	String pop();
+	boolean push(String ob);
+}
+
+class StringStack implements StackInterface {
+	private String str[] = new String[5];
+	private static int top = 0;
+	
+	public int length() {
+		return str.length;
+	}
+	public String pop() {
+		return str[--top];
+	}
+	public boolean push(String ob) {
+		if(top == str.length) {
+			return false;
+		}
+		
+		str[top++] = ob;
+		return true;
+	}
+}
+
+public class ch5_5 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		StringStack stack = new StringStack();
+		
+		System.out.print(">> ");
+		for(int i = 0 ; i < 5 ; i++) {
+			stack.push(sc.next());
+		}
+		
+		for(int i = 0 ; i < 5 ; i++) {
+			System.out.print(stack.pop()+" ");
+		}
+		
+		sc.close();
+
+	}
+
+}
---
+++ Java/ch5/src/ex05_1.java
@@ -0,0 +1,37 @@
+class Point {
+	private int x, y;
+	
+	public void set(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public void showPoint() {
+		System.out.println("("+x+","+y+")");
+	}
+}
+class ColorPoint extends Point {
+	private String color;
+	
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public void showColorPoint() {
+		System.out.print(color);
+		showPoint();
+	}
+}
+
+public class ex05_1 {
+
+	public static void main(String[] args) {
+		Point p = new Point();
+		p.set(3, 2);
+		p.showPoint();
+		
+		ColorPoint cp = new ColorPoint();
+		cp.set(1, 2);
+		cp.setColor("red");
+		cp.showColorPoint();
+	}
+
+}
